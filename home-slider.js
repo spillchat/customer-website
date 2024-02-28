324e7c20bbf83be0973aea5e0c97ceeab1e34252
kudzaimhlanga <37145094+kudzaimhlanga@users.noreply.github.com>
2024-02-28 09:38:56 +0200

    Update home slider
    Added navigation

--- home-slider.js
+++ home-slider.js
@@ -4,10 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
       effect: "slide",
       spaceBetween: 30,
       direction: "horizontal",
-      slideActiveClass: "is-active",
       slideToClickedSlide: true,
       slidesPerView: "auto",
       loop: true,
-      keyboard: true
+      keyboard: true,
+      navigation: {
+        nextEl: document.querySelector(".swiper-slide-button.is-test.is-next"),
+        prevEl: document.querySelector(".swiper-slide-button.is-test.is-prev")
+      }
     });
   });
